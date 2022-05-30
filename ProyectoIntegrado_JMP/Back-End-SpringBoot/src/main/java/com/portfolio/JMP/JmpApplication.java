70e1e22731fee271dcbe0d7a2a7b3f244178a7cf
juanmatiaspinat <juanmatiaspinat@gmail.com>
2022-05-29 22:18:18 -0300

    Creando backend

--- ProyectoIntegrado_JMP/Back-End-SpringBoot/src/main/java/com/portfolio/JMP/JmpApplication.java
+++ ProyectoIntegrado_JMP/Back-End-SpringBoot/src/main/java/com/portfolio/JMP/JmpApplication.java
@@ -1,4 +1,4 @@
-package com.portfolio.JMP;
+package com.portfolio.jmp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- ProyectoIntegrado_JMP/Back-End-SpringBoot/src/test/java/com/portfolio/JMP/JmpApplicationTests.java
+++ ProyectoIntegrado_JMP/Back-End-SpringBoot/src/test/java/com/portfolio/JMP/JmpApplicationTests.java
@@ -1,4 +1,4 @@
-package com.portfolio.JMP;
+package com.portfolio.jmp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
