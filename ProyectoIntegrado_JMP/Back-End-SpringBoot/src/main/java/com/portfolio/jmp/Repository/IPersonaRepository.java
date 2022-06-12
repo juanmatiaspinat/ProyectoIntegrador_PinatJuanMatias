65faf9d44859a4319f04094dfda7f3c731b15133
juanmatiaspinat <juanmatiaspinat@gmail.com>
2022-06-11 23:52:27 -0300

    Agregando cosas al backend

---
+++ ProyectoIntegrado_JMP/Back-End-SpringBoot/src/main/java/com/portfolio/jmp/Repository/IPersonaRepository.java
@@ -0,0 +1,10 @@
+package com.portfolio.jmp.Repository;
+
+import com.portfolio.jmp.Entity.Persona;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IPersonaRepository extends JpaRepository<Persona, Long> {
+    
+}
---
+++ ProyectoIntegrado_JMP/Back-End-SpringBoot/src/main/java/com/portfolio/jmp/Service/ImpPersonaService.java
@@ -0,0 +1,36 @@
+package com.portfolio.jmp.Service;
+
+import com.portfolio.jmp.Entity.Persona;
+import com.portfolio.jmp.Interface.IPersonaService;
+import com.portfolio.jmp.Repository.IPersonaRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ImpPersonaService implements IPersonaService{
+    @Autowired IPersonaRepository ipersonarepository;
+
+    @Override
+    public List<Persona> getPersona() {
+        List<Persona> persona = ipersonarepository.findAll();
+        return persona;
+    }
+
+    @Override
+    public void savePersona(Persona persona) {
+        ipersonarepository.save(persona);
+    }
+
+    @Override
+    public void deletePersona(Long id) {
+        ipersonarepository.deleteById(id);
+    }
+
+    @Override
+    public Persona findPersona(Long id) {
+        Persona persona = ipersonarepository.findById(id).orElse(null);
+        return persona;
+    }
+    
+}
